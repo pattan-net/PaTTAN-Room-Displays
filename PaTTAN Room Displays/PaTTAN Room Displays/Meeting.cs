ac5088bf839c7ad48fc651882b67121f90d8e5da
Markus Weltin <mweltin@gmail.com>
2022-01-10 14:35:26 -0800

    add Meeting class
    these objets will be used to create individual meetings to place into a
    iterable List

---
+++ PaTTAN Room Displays/PaTTAN Room Displays/Meeting.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaTTAN_Room_Displays
+{
+    internal class Meeting
+    {
+        DateTime startTime { get; set; }
+        DateTime endTime { get; set; }
+        String title { get; set; }
+        String roomName { get; set; }
+        String contact { get; set; }
+
+        public Meeting(String inContact, String inRoomName, String inTitle, DateTime inStartTime, DateTime inEndtime)
+        {
+            startTime = inStartTime;
+            endTime = inEndtime;
+            title = inTitle;
+            roomName = inRoomName;
+            contact = inContact;
+        }
+    }
+}
